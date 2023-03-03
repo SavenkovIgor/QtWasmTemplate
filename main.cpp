90619c0d9886eff9c5f879cb897edba21499b541
Savenkov Igor <savenkovigor.dev@gmail.com>
2023-03-03 16:11:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:/untitled/main.qml"_qs);
+    const QUrl url(u"qrc:/qt/qml/main.qml"_qs);
     QObject::connect(
         &engine, &QQmlApplicationEngine::objectCreated,
         &app, [url](QObject *obj, const QUrl &objUrl)
