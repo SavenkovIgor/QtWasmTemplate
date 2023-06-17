410247fd525338143d9dffb4704b0689a3dd5e18
Savenkov Igor <savenkovigor.dev@gmail.com>
2023-06-17 18:02:48 +0300

    Modernize qml loading (#5)
    * Switch to modern qml loading way
    
    * Condition fix

--- main.cpp
+++ main.cpp
@@ -6,15 +6,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:/qt/qml/QtTemplateModule/main.qml"_qs);
-    QObject::connect(
-        &engine, &QQmlApplicationEngine::objectCreated,
-        &app, [url](QObject *obj, const QUrl &objUrl)
-        {
-            if (!obj && url == objUrl)
-                QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.load(url);
+    engine.loadFromModule("QtTemplateModule", "Main");
 
     return app.exec();
 }
